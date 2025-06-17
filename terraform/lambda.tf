5a1396f3e5d34d458416716d05b33648cb115a24
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-06-17 12:57:50 -0230

    Add Lambda function for processing video metadata
    Added handler to fetch video metadata from RapidAPI
    
    Stores data in DynamoDB table safetube_videos
    
    Uses axios and AWS SDK
    
    Reads RAPID_API_KEY from environment variables

---
+++ terraform/lambda.tf
@@ -0,0 +1,19 @@
+resource "aws_lambda_function" "process_video" {
+  function_name = "processVideo"
+  role          = aws_iam_role.lambda_exec_role.arn
+
+  handler       = "index.handler"
+  runtime       = "nodejs20.x"
+
+  s3_bucket     = "safetube-lambda-deployments"    
+  s3_key        = "processVideo.zip"            
+
+  environment {
+    variables = {
+      RAPID_API_KEY = var.rapid_api_key
+    }
+  }
+
+  timeout      = 30
+  memory_size  = 128
+}
