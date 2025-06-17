0a17f6d28f32da15242fdde3619d387467fcf82e
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-06-17 21:06:02 -0230

    Add output for API Gateway invoke URL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "safetube_videos_table_arn" {
   value = aws_dynamodb_table.safetube_videos.arn
 }
+
+output "api_invoke_url" {
+  value = "https://${aws_api_gateway_rest_api.safetube_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.prod.stage_name}"
+}
