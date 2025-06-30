8074639d1c349bdede1badb96d62a26eabf14dd9
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-06-30 10:50:20 -0230

    add IAM roles and policies for ECS Fargate

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -59,3 +59,70 @@ resource "aws_iam_role_policy_attachment" "lambda_exec_policy_attach" {
   role       = aws_iam_role.lambda_exec_role.name
   policy_arn = aws_iam_policy.lambda_exec_policy.arn
 }
+
+# ============
+
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "safetube-ecs-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role" "ecs_task_role" {
+  name = "safetube-ecs-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_policy" "safetube_task_permissions" {
+  name = "safetube-task-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["sqs:ReceiveMessage", "sqs:DeleteMessage", "sqs:GetQueueAttributes"],
+        Resource = "*"  # You can limit to the specific queue ARN if you prefer
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["s3:PutObject"],
+        Resource = "arn:aws:s3:::${var.s3_bucket_name}/*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = ["logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_task_permissions" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.safetube_task_permissions.arn
+}
