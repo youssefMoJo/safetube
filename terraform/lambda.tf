6b392981543185c37679c66dd113db47850735e3
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-07-04 20:40:36 -0230

    add trigger_ecs function and SQS integration

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -21,3 +21,33 @@ resource "aws_lambda_function" "process_video" {
   timeout     = 30
   memory_size = 128
 }
+
+resource "aws_lambda_function" "trigger_ecs" {
+  function_name = "triggerECSTaskFromSQS"
+  role          = aws_iam_role.lambda_exec_role.arn
+
+  handler = "index.handler"
+  runtime = "nodejs20.x"
+
+  filename         = "${path.module}/../lambda/triggerECS/triggerECS.zip"
+  source_code_hash = filebase64sha256("${path.module}/../lambda/triggerECS/triggerECS.zip")
+
+  timeout     = 60
+  memory_size = 256
+
+  environment {
+    variables = {
+      ECS_CLUSTER       = aws_ecs_cluster.safetube_cluster.name
+      ECS_TASK_DEFINITION = aws_ecs_task_definition.safetube_task.arn
+      SUBNETS           = join(",", var.subnet_ids)
+      SECURITY_GROUP    = var.security_group_id
+    }
+  }
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_to_trigger_ecs" {
+  event_source_arn  = aws_sqs_queue.video_processing_queue.arn
+  function_name     = aws_lambda_function.trigger_ecs.arn
+  batch_size        = 1
+  enabled           = true
+}
