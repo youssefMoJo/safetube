014125ddfb251b1fdeb8957c7195355479f0012a
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-07-05 19:09:35 -0230

    Increase the backend progress from 60% to 70%

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -63,7 +63,7 @@ function App() {
           <CountdownTimer targetDate="2025-08-01T00:00:00" />
         </TimerWrapper>
         <ProgressWrapper>
-          <ProgressBar backendPercent={60} frontendPercent={10} />
+          <ProgressBar backendPercent={70} frontendPercent={10} />
           <GitHubRepo />
           <ArchitectureScrollCard />
         </ProgressWrapper>
