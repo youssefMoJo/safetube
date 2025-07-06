4d28614bd8db51cd6678661bcfc864cdbd145e79
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-07-06 14:15:30 -0230

    add parents and children DynamoDB tables

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -14,3 +14,37 @@ resource "aws_dynamodb_table" "safetube_videos" {
     Project     = "SafeTube"
   }
 }
+
+resource "aws_dynamodb_table" "safetube_parents" {
+  name         = "safetube_parents"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "parent_id"
+
+  attribute {
+    name = "parent_id"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "SafeTube"
+  }
+}
+
+resource "aws_dynamodb_table" "safetube_children" {
+  name         = "safetube_children"
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "child_id"
+
+  attribute {
+    name = "child_id"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "SafeTube"
+  }
+}
