1b6e6477340f45588756fa417954697e0e575824
Youssef Mohamed <48146406+youssefMoJo@users.noreply.github.com>
2025-07-09 17:34:50 -0230

    add Cognito user pool, user pool client, and user groups

---
+++ terraform/cognito.tf
@@ -0,0 +1,45 @@
+resource "aws_cognito_user_pool" "safetube_user_pool" {
+  name = "safetube-user-pool"
+
+  password_policy {
+    minimum_length    = 8
+    require_lowercase = true
+    require_numbers   = true
+    require_symbols   = false
+    require_uppercase = false
+  }
+
+  auto_verified_attributes = ["email"]
+
+  tags = {
+    Environment = "dev"
+    Project     = "SafeTube"
+  }
+}
+
+resource "aws_cognito_user_pool_client" "safetube_user_pool_client" {
+  name         = "safetube-user-pool-client"
+  user_pool_id = aws_cognito_user_pool.safetube_user_pool.id
+
+  generate_secret = false
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+  ]
+
+  prevent_user_existence_errors = "ENABLED"
+}
+
+resource "aws_cognito_user_group" "parents_group" {
+  user_pool_id = aws_cognito_user_pool.safetube_user_pool.id
+  name         = "Parents"
+  description  = "Group for parent accounts"
+}
+
+resource "aws_cognito_user_group" "children_group" {
+  user_pool_id = aws_cognito_user_pool.safetube_user_pool.id
+  name         = "Children"
+  description  = "Group for child accounts"
+}
